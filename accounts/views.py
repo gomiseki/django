00db1290834dbc1fe32d50a61bfc5be508e8a70f
hwonident <hwonident@gmail.com>
2020-07-21 16:46:57 +0900

    account

---
+++ accounts/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
+
+# Create your views here.
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(request, username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('mainpage')
+        #로그인 실패시 팝업창 띄우기
+
+    return render(request, 'login.html')
+
+def signup(request):
+    if request.method == 'POST':
+        if request.POST['password1'] == request.POST['password2']:
+            user = User.objects.create_user(
+                username = request.POST['username'], password=request.POST['password1'])
+            auth.login(request, user)
+            return redirect('login')
+    return render(request, 'signup.html')
